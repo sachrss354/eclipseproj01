11565721feb8c25960cccd5c8694c2e32f029622
Sachchidanand <Sachchidanand@exammportal>
2024-05-02 07:46:36 +0530

    TL DELTED B.JAVA git

--- src/main/java/com/nt/proj/App.java
+++ src/main/java/com/nt/proj/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public void X() {
+    	int a=10;
+    	System.out.println(a);
+    }
+    public void mul() {
+    	int x=12;
+    	int z=13;
+    	System.out.println(x*z);
+    }
+    public void div() {
+    	System.out.println((134.0/12));
+    }
+
 }
